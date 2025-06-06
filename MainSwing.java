56723b5f94ce10ce6b10ab6caa519b190e20d671
Kristinamatias <kristinamatias1@gmail.com>
2025-06-06 09:30:34 +0300

    Update MainSwing.java

--- MainSwing.java
+++ MainSwing.java
@@ -9,25 +9,25 @@ public class MainSwing {
     private static JTextArea outputArea = new JTextArea(15, 40);
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Туристическа система");
+        JFrame frame = new JFrame("Travel system");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(0, 1, 5, 5));
 
-        JButton loadButton = new JButton("Зареди от файл");
-        JButton showAllButton = new JButton("Покажи всички");
-        JButton searchButton = new JButton("Търси по държава");
-        JButton sortPriceButton = new JButton("Сортирай по цена");
-        JButton saveButton = new JButton("Запази във файл");
+        JButton loadButton = new JButton("Load from file");
+        JButton showAllButton = new JButton("Show all");
+        JButton searchButton = new JButton("Search by country");
+        JButton sortPriceButton = new JButton("Sort by price");
+        JButton saveButton = new JButton("Save to files");
 
         loadButton.addActionListener((ActionEvent e) -> {
             try {
                 manager.loadFromFile("trips.txt");
-                outputArea.setText("Заредено успешно!");
+                outputArea.setText("Saved successfully!");
             } catch (Exception ex) {
-                outputArea.setText("Грешка при зареждане.");
+                outputArea.setText("Loading error.");
             }
         });
 
@@ -39,7 +39,7 @@ public class MainSwing {
         });
 
         searchButton.addActionListener((ActionEvent e) -> {
-            String country = JOptionPane.showInputDialog("Въведи държава:");
+            String country = JOptionPane.showInputDialog("Enter country:");
             List<Trip> results = manager.searchByCountry(country);
             outputArea.setText("");
             for (Trip t : results) {
@@ -49,15 +49,15 @@ public class MainSwing {
 
         sortPriceButton.addActionListener((ActionEvent e) -> {
             manager.sortByPrice();
-            outputArea.setText("✔ Сортирано по цена.");
+            outputArea.setText("✔ Sorted by price.");
         });
 
         saveButton.addActionListener((ActionEvent e) -> {
             try {
                 manager.saveToFile("trips.txt");
-                outputArea.setText("Записано успешно.");
+                outputArea.setText("Writen successfully.");
             } catch (Exception ex) {
-                outputArea.setText("Грешка при запис.");
+                outputArea.setText("Write error.");
             }
         });
 
